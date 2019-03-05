9fdad8c41ec23d522a3756085f9bf6eb19f13c9d
malusarea06 <malusarea111@gmail.com>
2019-03-05 12:19:46 +0530

    Execute all feature

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -32,7 +32,7 @@ import utilities.ExtentReport;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
-		features="Feature/MyFeature.feature",
+		features="Feature/",
 		glue= {"stepDefinition"},
 		plugin= {"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}
 		
